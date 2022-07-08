ef82391b9d5b4766efb040a884b166a1dccb7a39
faris771 <farisabufarha33@gmail.com>
2022-07-08 15:19:48 +0300

    features

--- login.js
+++ login.js
@@ -2,15 +2,17 @@ function validate() {
 
     var username = document.getElementById("uname").value;
     var passwd = document.getElementById("passwd").value;
+    var gender = document.getElementById("gender").value;
 
-    if (username == "v=ir" && passwd == "v=ir") {
+
+    if (username == "v=ir" && passwd == "v=ir" && gender == "شعبة الصبح") {
 
         alert("WELCOME");
         return false;
 
 
     } else {
-        alert("INVALID USERNAME OR PASSWORD, HINT: BEST FORMULA ??")
+        alert("INVALID USERNAME, PASSWORD, OR GENDER \nHINT: BEST FORMULA ??")
 
     }
 
